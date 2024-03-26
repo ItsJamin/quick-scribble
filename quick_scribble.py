df4ded34276fc2aa47e22f2b2fcb21f5a8c12849
ItsJamin <jamonjamin4@gmail.com>
2024-03-26 17:39:56 +0100

    Alpha Value of Background Toggle

--- quick_scribble.py
+++ quick_scribble.py
@@ -4,8 +4,15 @@ import tkinter as tk
 pencil_size = 8
 previous_point, current_point = [0,0],[0,0]
 
-def on_resize(event):
-    canvas.config(width=event.width, height=event.height)
+# Window-Relevant Variables
+width = 600
+height = 400
+screen_width = None
+screen_height = None
+resize_after_id = None
+
+alpha_values = [0.2, 0.6, 1.0]
+current_alpha_index = 0
 
 def _stop_draw(event):
     global previous_point
@@ -26,6 +33,12 @@ def _draw(event, size = 8, color = "black"):
     previous_point = current_point
 
 
+def _toggle_alpha(event):
+    global current_alpha_index
+    current_alpha_index = (current_alpha_index + 1) % len(alpha_values)
+    new_alpha = alpha_values[current_alpha_index]
+    root.attributes("-alpha", new_alpha)
+
 def _clear_canvas():
     global canvas
     canvas.delete("all")
@@ -37,19 +50,13 @@ root.title("Quick Scribble")
 
 screen_width = root.winfo_screenwidth()/2
 screen_height = root.winfo_screenheight()/2
-width = 600
-height = 400
 root.geometry(f"{width}x{height}+{int(screen_width-width/2)}+{int(screen_height-height/2)}")
 root.attributes("-alpha", 0.8)
 
 canvas = tk.Canvas(root, bg="white")
 canvas.pack(fill=tk.BOTH, expand=True)
-canvas.grid(row=0, column=0, sticky="nsew")
 
-root.grid_rowconfigure(0, weight=1)
-root.grid_columnconfigure(0, weight=1)
 
-#window control
 
 #gives extra parameters through lambda function to cdraw functions
 canvas.bind("<B1-Motion>", _draw) 
@@ -58,8 +65,7 @@ canvas.bind("<ButtonRelease-1>", _stop_draw)
 canvas.bind("<B3-Motion>", lambda event: _draw(event, pencil_size*2, "white"))  # Right-click to erase
 canvas.bind("<ButtonPress-3>", lambda event: _draw(event, pencil_size*2, "white"))
 canvas.bind("<ButtonRelease-3>", _stop_draw)
-canvas.bind("<Configure>", on_resize)
-
+root.bind("<Tab>", _toggle_alpha)
 
 # Start the Interface Loop
 root.mainloop()
